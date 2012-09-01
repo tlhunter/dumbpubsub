70b044a5a6a0f6b851c39b0a1cfafb2e645bba60
Thomas Hunter <tlhunter@gmail.com>
2012-09-01 16:03:52 -0400

    documentations

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var pubsub = require('dumbpubsub');
 var express = require('express');
 
-var app = express();
-pubsub.attach(app);
+var app = express(); // Create an Express app
+pubsub.attach(app); // DumbPubSub will now use the existing Express app
 
 app.get('/', function(req, res) {
     res.send('hello world');
 });
 
-pubsub.listen('/subscribe');
+pubsub.listen('/subscribe'); // The DumbPubSub listen method defines the root URL
 
 app.listen(3000);
 
