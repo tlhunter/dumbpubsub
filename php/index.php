fab02ca9c995a290fcf68fca9f8d32caae469022
Thomas Hunter <TomHunter@QuickenLoans.com>
2012-09-05 11:15:10 -0400

    Example PHP script

---
+++ php/index.php
@@ -0,0 +1,7 @@
+GET: <?=$_GET['type']?>
+
+<pre><?php print_r($_POST); ?></pre>
+
+This is a simple script to show the results of a POST operation. It could be
+something more complex like running important scripts, updating cache, or
+sending emails.
