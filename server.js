c8c0cbcf4dac4f9432d9df3552f357763ec004ca
Thomas Hunter <TomHunter@QuickenLoans.com>
2012-09-07 14:11:41 -0400

    grouping dumb commands

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var dumb = require('dumbpubsub');
 var express = require('express');
 
 var app = express(); // Create an Express app
-dumb.attach(app); // DumbPubSub will now use the existing Express app
 
+dumb.attach(app); // DumbPubSub will now use the existing Express app
 dumb.notifyEvent(); // By default, we don't tell client what was run, we assume their URL will let them know
 dumb.persistOnExit(); // Makes sure we save all subscriptions to disk when we quit
+dumb.restore('subscriptions.json');
+dumb.listen('/subscribe'); // The DumbPubSub listen method defines the root URL
 
 // Normal application requests work as expected
 app.get('/', function(req, res) {
@@ -21,9 +23,6 @@ app.get('/trigger/:event', function(req, res) {
 });
 
 // One day we will pass in a redis or mongodb connection, but for now just a filename
-dumb.restore('subscriptions.json');
-
-dumb.listen('/subscribe'); // The DumbPubSub listen method defines the root URL
 
 app.listen(3000); // Express (and DumbPubSub) both listen on the same port
 console.log('Listening for incomming HTTP requests.');
